f27a6ec8c8f3bbcea9e92329b31ff305caaed824
Fabrice Aneche <akh@inair.space>
2020-05-04 22:35:47 -0400

    extracting categories from ny2o

---
+++ extract_tools/categories.py
@@ -0,0 +1,28 @@
+import urllib.request
+import collections
+from bs4 import BeautifulSoup
+import simplejson as json
+
+categories = {}
+
+Category = collections.namedtuple('Category', ['title', 'sats'])
+
+for i in range(2,53):
+    print(i)
+    f = urllib.request.urlopen('https://www.n2yo.com/satellites/?c=' + str(i))
+    data = f.read().decode('utf-8')
+    soup = BeautifulSoup(data, "html.parser")
+    title = soup.find('h1').text
+
+    sats = []
+    tables = soup.find_all('table')
+    for tr in tables[1].find_all('tr'):
+        row = tr.find_all('td')
+        if len(row) == 6:
+            sat = int(int(row[1].text))
+            sats.append(sat)
+
+    categories[i] = Category(title, sats)
+
+with open('categories.json', 'w') as f:
+    json.dump(categories, f)
