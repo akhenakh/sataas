ec974c763e66a4ba916d8dd404417990dd4b7cac
Fabrice Aneche <akh@inair.space>
2020-05-04 22:49:42 -0400

    nicer categories name

--- extract_tools/categories.py
+++ extract_tools/categories.py
@@ -3,17 +3,17 @@ import collections
 from bs4 import BeautifulSoup
 import simplejson as json
 
-categories = {}
+categories = []
 
-Category = collections.namedtuple('Category', ['title', 'sats'])
+Category = collections.namedtuple('Category', ['id', 'name', 'sats'])
 
-for i in range(2,53):
+for i in range(2, 53):
     print(i)
     f = urllib.request.urlopen('https://www.n2yo.com/satellites/?c=' + str(i))
     data = f.read().decode('utf-8')
     soup = BeautifulSoup(data, "html.parser")
     title = soup.find('h1').text
-
+    title = title.replace(' SATELLITES', '')
     sats = []
     tables = soup.find_all('table')
     for tr in tables[1].find_all('tr'):
@@ -22,7 +22,7 @@ for i in range(2,53):
             sat = int(int(row[1].text))
             sats.append(sat)
 
-    categories[i] = Category(title, sats)
+    categories.append(Category(i, title, sats))
 
 with open('categories.json', 'w') as f:
-    json.dump(categories, f)
+    json.dump(categories, f, sort_keys=True, indent=4 * ' ')
